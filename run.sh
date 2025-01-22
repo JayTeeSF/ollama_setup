dbab234728596b681607916ad837d20655fbc995
JayTeeSF <jaytee@jayteesf.com>
2025-01-21 22:15:33 -0800

    Run command uses llama3.2 by default; cuz the 7B deepseek-r1 is meh...

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
+OLLAMA_MODEL="$1"
+# OLLAMA_MODEL="llama3.2" #deepseek-r1"
+if [ -z "$OLLAMA_MODEL" ]; then
+  OLLAMA_MODEL="llama3.2"
+  echo "Usage: $0 <model: ${OLLAMA_MODEL} || deepseek-r1>"
+  # exit 1
+fi
+export OLLAMA_MODEL
 docker-compose -f docker-compose.ollama.yml -f docker-compose.open_web_ui.yml up -d
